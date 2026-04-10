7723ed7612d022a19a13475c4d7e4426a10aa223
auau1662fr <auric.chrn.2005@gmail.com>
2026-04-10 14:26:01 +0200

    add config.py

--- part3/config.py
+++ part3/config.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///hbnb.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
