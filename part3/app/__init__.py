e343909af10fad62476f70f9bffae19cb9a54b26
auau1662fr <auric.chrn.2005@gmail.com>
2026-04-10 11:55:42 +0200

    add  __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.extensions import db, bcrypt, jwt
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +12,7 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+    CORS(app)
 
     from app.api.v1 import api_v1
     app.register_blueprint(api_v1)
